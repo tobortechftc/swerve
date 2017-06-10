71990406b1d08f78b56f9a2fc53fc14c87e6e84b
NickDeGrood <nick.degrood@gmail.com>
2017-06-09 20:50:47 -0700

    beep boop i am a bot and mason is a nerd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDriveTeleop.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-//
+
 
 @TeleOp(name="SwerveDrive: Teleop", group="SwerveDrive")
 public class SwerveDriveTeleop extends LinearOpMode {
