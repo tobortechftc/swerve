badd1370d3741d3fb93e2fe1b7ea4a60cbe5979a
NickDeGrood <nick.degrood@gmail.com>
2017-09-22 21:06:44 -0700

    Put in values for red and blue balls, but not the logic for using them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorChooser.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorChooser.java
@@ -3,38 +3,42 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 /**
  * Created by Nick on 9/16/2017.
  */
-@Autonomous(name = "Swerve: ColorChooser", group = "Swerve")
+@TeleOp(name = "Swerve: ColorChooser", group = "Swerve")
 public class ColorChooser extends LinearOpMode {
 
     ColorSensor colorSensor;
-    Servo armYaw;
-    Servo armPitch;
-    final static double RED = 6.0;  // These values are just placeholders
-    final static double BLUE = 4.0;
+    Servo armYaw;   // side to side
+    Servo armPitch; // up and down
+    final static int RED_BALL_MIN = -89; // NOTE: DO RED - BLUE
+    final static int RED_BALL_MAX = -31;
+    final static int BLUE_BALL_MIN = 17;
+    final static int BLUE_BALL_MAX = 61;
     final static double CHOOSE_RED = 1.0;
     final static double CHOOSE_BLUE = 0.0;
+
 
     @Override
     public void runOpMode() {
         colorSensor = hardwareMap.colorSensor.get("colorSensor");
-        armYaw = hardwareMap.servo.get("armAngle");
+//        armYaw = hardwareMap.servo.get("armYaw");
 
-        armPitch.setPosition(0.0);
+
+        waitForStart();
+
+//        armPitch.setPosition(0.0);
         while (opModeIsActive()) {
-            if (colorSensor.red() >= RED) { // The color sensor sees red and angles arm to hit red jewel
-                armYaw.setPosition(CHOOSE_RED);
-            }
-            else if (colorSensor.blue() >= BLUE) { // The color sensor sees blue and angles arm to hit blue jewel
-                armYaw.setPosition(CHOOSE_BLUE);
-            }
-            else { // The color sensor didn't see red nor blue
-                // well crap
-            }
+            telemetry.addData("Clear", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+            telemetry.update();
+            sleep(100);
         }
     }
 }
