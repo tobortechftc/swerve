cd8c79c1ab0a1892dcc7ed182962baf4789b490d
kunhantsai <kunhantsai@gmail.com>
2017-12-09 17:04:36 -0800

    Revert "Change all drive functions to have the relative back wheels trail at zero power"
    This reverts commit d155af4c425c6921ad785183ac6afafa97bb7066.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueSideBox.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueSideBox.java
@@ -58,7 +58,16 @@ public class AutoBlueSideBox extends SwerveUtilLOP{
                 //turnToColumn(robot.targetColumn, 0.2, true, true);
                 go_to_distance_from(0.3, robot.targetColumn, true, true, true); // Drive to cryptobox.
                 TurnLeftD(0.3, 80);
-                deliverGlyph();
+                StraightIn(0.5, 5);
+                glyph_grabber_auto_open();
+                StraightIn(-0.4, 7);
+                glyph_grabber_half_close();
+                glyph_slider_back_init();
+                sleep(500);
+                StraightIn(0.4, 10);
+                sleep(100);
+                StraightIn(-0.4, 3);
+                glyph_grabber_auto_open();
                 stop_chassis();
             } catch (Exception e) {
                 StringWriter sw = new StringWriter();
