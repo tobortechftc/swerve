4397b6cb1ad10aa57a131d33ed931416ad1e8d01
Auror4 <masonmann2020@gmail.com>
2018-01-12 18:50:42 -0800

    Fixed Position Diagnostics Infinite Loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionDiagnostics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionDiagnostics.java
@@ -64,11 +64,12 @@ public class PositionDiagnostics extends SwerveUtilLOP {
                 }
                 if (robot.use_Vuforia) {
                     int pictograph = get_cryptobox_column();
-                    while (pictograph == -1)
+                    while (pictograph == -1) {
                         telemetry.addData("Pictograph Not Visible!", null);
-                    sleep(200);
-                    pictograph = get_cryptobox_column();
-                    sleep(100);
+                        sleep(200);
+                        pictograph = get_cryptobox_column();
+                        sleep(100);
+                    }
                     if (pictograph != -1) {
                         telemetry.addData("Pictograph visible", null);
                     }
